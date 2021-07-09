9ad0d7a3fe73385c4babdd0e5f3d54467b3fcd61
Jeremi Rodas <jrodas-2019129@kinal.edu.gt>
2021-07-09 06:42:43 -0600

    cambio heroku y mongoDB

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = require("./app")
 var UsuarioControlador = require("./src/controladores/usuarios.controlador")
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/ProyectoDeportivo', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect('mongodb+srv://Admin:Admin@proyectodeportivo.0gxmz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Se encuentra conectado a la base de datos');
 
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log("Servidor corriendo en el puerto 3000");
         UsuarioControlador.adminApp();
     })
